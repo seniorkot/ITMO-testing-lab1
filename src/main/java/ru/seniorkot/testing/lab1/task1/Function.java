2893f4877f4b3a4cc51289e2196b52493e3be2fd
seniorkot <seniorkot24@yandex.ru>
2018-03-31 00:09:53 +0300

    1 task: Function written

--- src/main/java/ru/seniorkot/testing/lab1/task1/Function.java
+++ src/main/java/ru/seniorkot/testing/lab1/task1/Function.java
@@ -1,4 +1,26 @@
 package ru.seniorkot.testing.lab1.task1;
 
 public class Function {
+    private static double doubleFact(double n){
+        if (n <= 0) {
+            return 1;
+        }
+        return (n-1) / n * doubleFact(n-2);
+    }
+
+    public static double arcsin(double x, double eps){
+        if (Math.abs(x) > 1.0){
+            return Double.NaN;
+        }
+        int n = 1;
+        double result = x;
+        double prevResult;
+        do {
+            prevResult = result;
+            double fac = doubleFact(2 * n);
+            result += fac * Math.pow(x, 2 * n + 1) / (2 * n + 1);
+            n++;
+        } while (Math.abs(result - prevResult) > eps);
+        return result;
+    }
 }
--- src/test/java/ru/seniorkot/testing/lab1/task1/FunctionTest.java
+++ src/test/java/ru/seniorkot/testing/lab1/task1/FunctionTest.java
@@ -1,4 +1,11 @@
 package ru.seniorkot.testing.lab1.task1;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FunctionTest {
+
+    private static final double EPS = 1E-7;
+    private static final double DELTA = 1E-6;
+
 }
