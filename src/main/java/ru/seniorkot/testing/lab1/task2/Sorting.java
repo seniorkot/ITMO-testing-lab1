c9af050ac9df2373c666de68f68994f45263ac72
seniorkot <seniorkot24@yandex.ru>
2018-03-31 06:09:27 +0300

    task 2: Sorting Algorithms class created

---
+++ src/main/java/ru/seniorkot/testing/lab1/task2/Sorting.java
@@ -0,0 +1,34 @@
+package ru.seniorkot.testing.lab1.task2;
+
+public class Sorting {
+
+    public static int[] insertionSort(int[] a){
+        return null;
+    }
+
+    public static int[] selectionSort(int[] a){
+        return null;
+    }
+
+    public static int[] bubbleSort(int[] a){
+        return null;
+    }
+
+    public static int[] quickSort(int[] a){
+        return null;
+    }
+
+    public static int[] mergeSort(int[] a){
+        return null;
+    }
+
+    public static int[] shellSort(int[] a){
+        return null;
+    }
+
+    private static void swap(int[] a, int i, int j){
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
