0d0f2e065bd6f5555cc742c6734957dc25298553
Dimortilus <Dimortiluss@yandex.ru>
2018-04-14 14:00:20 +0300

    task3 minor changes

--- src/test/java/ru/seniorkot/testing/lab1/task3/HereTest.java
+++ src/test/java/ru/seniorkot/testing/lab1/task3/HereTest.java
@@ -79,6 +79,11 @@ public class HereTest {
 
     @Test
     public void testHereIdeal() {
+
+        expectedException.expect(NullPointerException.class);
+        expectedException.expectMessage(
+                CoreMatchers.equalTo("implementation of AbstractHuman offers something inside Flacon but Flacon is empty"));
+
         here = new Here(new Ford(), new Arthur(), new DentrassiUnderwear(),
                 new SquornshellousMattress(), null,
                 new Flacon(Flacon.Material.GLASS), new Fish(Fish.Size.SMALL, Color.YELLOW));
